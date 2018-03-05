565c56aae8f583fb5f0d14006e1266761aad369d
howarde8 <howarde8@gmail.com>
2018-03-05 03:55:12 -0800

    Compelete client send msg to server

--- Team5Lab1/Q2/udp_client.c
+++ Team5Lab1/Q2/udp_client.c
@@ -1,6 +1,38 @@
+#include <arpa/inet.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-int main() {
-    printf("Hello World\n");
+#define UDP_DAYTIME_PORT 5678 // Default should be 12, but reserved by OS
+
+int main(int argc, char *argv[]) {
+
+    char msg[100];
+    if (argc != 2) {
+        printf("Please give input message\n");
+        return 1;
+    }
+
+    // Create socket
+    int sd = socket(AF_INET, SOCK_DGRAM, 0); // SOCK_DGRAM for UDP
+    if (sd < 1) {
+        printf("Couldn't create socket\n");
+        return 1;
+    }
+    printf("UDP socket created\n");
+
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr("127.0.0.1"); // only test for localhost
+    server.sin_port = htons(UDP_DAYTIME_PORT);
+
+    // Send message to server
+    if (sendto(sd, argv[1], sizeof(argv[1]), 0,
+        (struct sockaddr *)&server, sizeof(server)) < 0) {
+        printf("Sento failed\n");
+        return 1;
+    }
+    printf("Sent message %s to server\n", argv[1]);
+
     return 0;
 }
--- Team5Lab1/Q2/udp_server.c
+++ Team5Lab1/Q2/udp_server.c
@@ -1,6 +1,40 @@
+#include <arpa/inet.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-int main() {
-    printf("Hello Server\n");
+#define UDP_DAYTIME_PORT 5678 // Default should be 12, but reserved by OS
+
+int main(int argc, char *argv[]) {
+
+    // Create socket
+    int sd = socket(AF_INET, SOCK_DGRAM, 0); // SOCK_DGRAM for UDP
+    if (sd < 1) {
+        printf("Couldn't create socket\n");
+        return 1;
+    }
+    printf("UDP socket created\n");
+
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY; // enable any ip_addr
+    server.sin_port = htons(UDP_DAYTIME_PORT);
+
+    // Bind
+    if (bind(sd, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        printf("Bind failed\n");
+        return 1;
+    }
+    printf("Bind successful, listen to port %d\n", UDP_DAYTIME_PORT);
+
+    // Receive message
+    char msg[100];
+    if (recvfrom(sd, msg, sizeof(msg) - 1, 0, NULL, 0) < 0) {
+        printf("Recvfrom failed\n");
+        return 1;
+    }
+    printf("Recvfrom successful, message from client is \"%s\"\n", msg);
+
+
     return 0;
 }
