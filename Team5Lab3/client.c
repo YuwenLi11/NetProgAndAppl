6fb2e03accfb1c0117ba3eef62b418cce743bcfb
howarde8 <howarde8@gmail.com>
2018-04-10 00:54:57 -0700

    Complete client

--- Team5Lab3/client.c
+++ Team5Lab3/client.c
@@ -6,9 +6,14 @@
 #include <unistd.h>         // read, write
 
 #define PORT 5678
-#define MSG_SIZE 1024
+#define MSG_SIZE 2048
 
 int main() {
+    // User input file_name
+    char file_name[128];
+    printf("Please input file name to request: ");
+    scanf("%s", file_name);
+
     // Create socket
     int sd = socket(AF_INET, SOCK_STREAM, 0); // SOCK_STREAM for TCP
     if (sd < 1) {
@@ -32,7 +37,7 @@ int main() {
     // Write
     // Define message to be written
     char client_msg[MSG_SIZE] = {0};
-    strcpy(client_msg, "GET / HTTP/1.1\r\n\r\n");
+    sprintf(client_msg, "GET /%s HTTP/1.1\r\n\r\n", file_name);
     if (write(sd, client_msg, strlen(client_msg)) < 0) {
         printf("Write failed\n");
         return -1;
@@ -47,7 +52,23 @@ int main() {
         printf("Read failed\n");
         return -1;
     }
+
     printf("Read from server:\n%s\n", server_msg);
+    char file_from_server[MSG_SIZE] = {0};
+    read_size = read(sd, file_from_server, MSG_SIZE);
+    if (read_size < 0) {
+        printf("Read failed\n");
+        return -1;
+    }
+    if (strcmp(server_msg, "HTTP/1.1 200 OK\r\n\r\n") == 0) {
+        // write file
+        FILE *file_to_write;
+        file_to_write = fopen(file_name, "wb");
+        fwrite(file_from_server, read_size, 1, file_to_write);
+        fclose(file_to_write);
+    } else {
+        printf("Fail message from server:\n%s\n", file_from_server);
+    }
 
     return 0;
 }
